0c0e1deefe8235bfefc9d4d7cd6d1ed63a280f18
Marius Clipa <166024299+MariusClipa@users.noreply.github.com>
2025-01-25 16:12:59 +0100

    Create Bibliotheque_universites.py

---
+++ Bibliotheque_universites.py
@@ -0,0 +1,25 @@
+import class_universite
+
+
+class BibliothequeUniversite :
+    def __init__(self):
+        self.universite = []
+
+    def add_universite(self, new_universite):
+        universite = self.universite.append(new_universite)
+        return universite
+
+    def show_all_universities(self):
+        for universite in self.universite:
+            print(universite.details_universite())
+
+    def update_nota_for_universite(self, name, notaToUpdate):
+        global to_update_universite
+        for universite in self.universite:
+            if universite.name == name:
+                to_update_universite = universite
+                to_update_universite.update_nota(notaToUpdate)
+                print(f"La note de l'Universite a ete actualise")
+            else:
+                to_update_universite = None
+                print(f"erreur")
